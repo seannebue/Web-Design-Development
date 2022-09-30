579c938858066a7b810379e03d68d06239d0cc1c
Seanne Ebue <89885927+seannebue@users.noreply.github.com>
2022-09-30 21:42:10 +0800

    Add files via upload

--- Ebue_Exercise 1.php
+++ Ebue_Exercise 1.php
@@ -1,16 +1,19 @@
 <html>
-<head><title>Exercise</title></head>
-
+<head><title>Exercise 1</title></head>
 <?php
-echo "Hello, Good day! My name is Seanne Alexandra A. Ebue ";
-echo "with a course of B.S Computer Engineering,  ";
-echo "now a 3rd Year Student from the section of WDD-58043,  ";
-echo "and Today, I will learn PHP Programming.  ";
-echo "<h1>Seanne Alexandra A. Ebue </h1>";
-echo "<h1> Bachelor of Science in Computer Engineering </h1>";
-echo "<h1>3rd Year- WDD-58043</h1>";
-echo "Today, I will learn PHP Programming.";
-?> 
+
+    $FullName = " Seanne Alexandra A. Ebue  ";
+    echo $FullName;
+    echo "<br>";
+    $CollegeProgram = "BS Computer Engineering";
+    echo $CollegeProgram;
+    echo "<br>";
+    $YearandSection = "3rd Year - WDD-58043";
+    echo $YearandSection;
+    echo "<br>";
+    echo $string = "Today, I will learn PHP Programming";
+
+?>
 
 </body>
 </html>
